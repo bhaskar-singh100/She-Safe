6cb39876f728a3ad6ace6674d3f4dba4ad5a3d7b
Ayush Kumar Gupta <guptayush47@gmail.com>
2024-10-15 00:28:45 +0530

    feature: css update

--- src/components/main/Feature.jsx
+++ src/components/main/Feature.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 export default function Feature() {
   return (
     <div className='w-[70vw]'>
-        <h2 className='w-[60vw] text-5xl font-mono leading-snug my-10'>The product is built with user-centric design and unmatched performance at its core</h2>
+        <h2 className='w-[60vw] text-5xl font-serif leading-snug my-10'>The product is built with user-centric design and unmatched performance at its core</h2>
         <div 
             className=" 
-            bg-[url('https://miro.medium.com/v2/resize:fit:828/format:webp/0*g1v2gh2jbe5NX8k8')]
+            bg-[url('https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]
             
-             bg-cover rounded-2xl">
-            <div className='relative z-10 text-white p-16 text-2xl font-semibold'>
-                <div className='flex justify-between my-20'>
+            bg-cover rounded-2xl">
+            <div className='relative z-10 text-white p-6 text-2xl font-semibold'>
+                <div className='flex justify-between my-10 rounded-lg hover:text-black hover:backdrop-blur-3xl hover:shadow-2xl p-10'>
                     <h3>Feature 1</h3>
                     <div>
                         <span>Track Me and SOS</span>
@@ -19,7 +19,7 @@ export default function Feature() {
                     </div>
                 </div>
 
-                <div className='flex justify-between my-20'>
+                <div className='flex justify-between my-10 rounded-lg hover:text-black hover:backdrop-blur-3xl hover:shadow-2xl p-10'>
                     <h3>Feature 2</h3>
                     <div>
                         <span>Track Me and SOS</span>
@@ -29,7 +29,7 @@ export default function Feature() {
 
 
 
-                <div className='flex justify-between my-20'>
+                <div className='flex justify-between my-10 rounded-lg hover:text-black hover:backdrop-blur-3xl hover:shadow-2xl p-10'>
                     <h3>Feature 3</h3>
                     <div>
                         <span>Track Me and SOS</span>
@@ -38,7 +38,7 @@ export default function Feature() {
                 </div>
 
 
-                <div className='flex justify-between my-20'>
+                <div className='flex justify-between my-10 rounded-lg hover:text-black hover:backdrop-blur-3xl hover:shadow-2xl p-10'>
                     <h3>Feature 4</h3>
                     <div>
                         <span>Track Me and SOS</span>
@@ -47,7 +47,7 @@ export default function Feature() {
                 </div>
 
 
-                <div className='flex justify-between my-20'>
+                <div className='flex justify-between my-10 rounded-lg hover:text-black hover:backdrop-blur-3xl hover:shadow-2xl p-10'>
                     <h3>Feature 5</h3>
                     <div>
                         <span>Track Me and SOS</span>
--- src/components/main/FeatureDisc.jsx
+++ src/components/main/FeatureDisc.jsx
@@ -3,37 +3,37 @@ import second from '../../assets/Laptop.png'
 
 export default function FeatureDisc() {
   return (
-    <div className='w-[60vw]'>
+    <div className='w-[70vw]'>
         <h2 className='text-5xl my-12'>Feature Details 🦒</h2>
 
 
-        <div className='border border-neutral-800 p-6 rounded-2xl flex flex-col items-center my-10'>
+        <div className='border border-neutral-800  p-6 rounded-2xl flex flex-col items-center my-10 shadow-xl shadow-neutral-900'>
             <div className='flex justify-between'>
-            <h3 className='text-4xl font-semibold'>Feature 😊</h3>
+            <h3 className='text-2xl font-mono font-semibold'>Feature 😊</h3>
             <div className='w-1/2'>
-                <h3 className='text-3xl font-semibold mb-6 w-2/3'>Track Me and SOS and Everything going Good</h3>
+                <h3 className='text-2xl font-semibold mb-6 w-2/3 font-mono'>Track Me and SOS and Everything going Good</h3>
                 <p className='text-neutral-200 font-thin'>With insights from Canva, Loom, Amplitude and more. Discover when and how to build a high-performing Growth team Learn about key signals, team structures, and hiring criteria for success. In this chapter, we’ll cover:</p>
             </div>
         </div>
         <img src={second} alt="" className='w-2/3 mt-9'/>
         </div>
 
-        <div className='border border-neutral-800 p-6 rounded-2xl flex flex-col items-center my-10'>
+        <div className='border border-neutral-800  p-6 rounded-2xl flex flex-col items-center my-10 shadow-xl shadow-neutral-900'>
             <div className='flex justify-between'>
-            <h3 className='text-4xl font-semibold'>Feature 😊</h3>
+            <h3 className='text-2xl font-mono font-semibold'>Feature 😊</h3>
             <div className='w-1/2'>
-                <h3 className='text-3xl font-semibold mb-6 w-2/3'>Track Me and SOS and Everything going Good</h3>
+                <h3 className='text-2xl font-semibold mb-6 w-2/3 font-mono'>Track Me and SOS and Everything going Good</h3>
                 <p className='text-neutral-200 font-thin'>With insights from Canva, Loom, Amplitude and more. Discover when and how to build a high-performing Growth team Learn about key signals, team structures, and hiring criteria for success. In this chapter, we’ll cover:</p>
             </div>
         </div>
         <img src={second} alt="" className='w-2/3 mt-9'/>
         </div>
 
-        <div className='border border-neutral-800 p-6 rounded-2xl flex flex-col items-center my-10'>
+        <div className='border border-neutral-800  p-6 rounded-2xl flex flex-col items-center my-10 shadow-xl shadow-neutral-900'>
             <div className='flex justify-between'>
-            <h3 className='text-4xl font-semibold'>Feature 😊</h3>
+            <h3 className='text-2xl font-mono font-semibold'>Feature 😊</h3>
             <div className='w-1/2'>
-                <h3 className='text-3xl font-semibold mb-6 w-2/3'>Track Me and SOS and Everything going Good</h3>
+                <h3 className='text-2xl font-semibold mb-6 w-2/3 font-mono'>Track Me and SOS and Everything going Good</h3>
                 <p className='text-neutral-200 font-thin'>With insights from Canva, Loom, Amplitude and more. Discover when and how to build a high-performing Growth team Learn about key signals, team structures, and hiring criteria for success. In this chapter, we’ll cover:</p>
             </div>
         </div>
