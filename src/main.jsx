105511d41f5915b0abc8ff498571d0b1b77545fd
Ayush Kumar Gupta <guptayush47@gmail.com>
2024-10-29 22:37:37 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import {
-  BrowserRouter,
   RouterProvider,
   createBrowserRouter,
 } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import Navbar from "./components/Navbar.jsx";
 import Login from "./components/Login.jsx";
-import Hero from "./components/Hero.jsx";
-import Feature from "./components/Feature.jsx";
-import FeatureDisc from "./components/FeatureDisc.jsx";
-import Contact from "./components/Contact.jsx";
 import Signup from "./components/Signup.jsx";
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
   {
@@ -24,10 +19,7 @@ const router = createBrowserRouter([
         path: "/",
         element: (
           <>
-            <Hero />
-            <Feature />
-            <FeatureDisc />
-            <Contact />
+            <Home/>
           </>
         ),
       },
